7e21413d31d528a1e6e919e76a17df102f539c5a
Ryan Compton <ryan.wade.compton@gmail.com>
2020-01-15 17:46:42 -0700

    Built additional hour rows. Complete local storage functionality

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 1)cs
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //
+    //Assign saveBtn click listener for user input and time stamp.
     $(".saveBtn").on("click", function () {
         //get nearby vaules
         console.log(this);
@@ -14,20 +14,29 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 1)cs
     })
     //load any saved data from LocalStorage - do this for each hour created*******
     $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
 
     function hourTracker() {
         //get current number of hours
         var currentHour = moment().hours();
 
         // loop over time blocks
-        $(".time-block").each(function() {
+        $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("")[1]);//*****be weary of .split. May need to remove */
 
             //check if we've moved past this time
             if (blockHour < currentHour) {
                 $(this).addClass("past");
             }
-            else if(blockHour === currentHour) {
+            else if (blockHour === currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
             }
